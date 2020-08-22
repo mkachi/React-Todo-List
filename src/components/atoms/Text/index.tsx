eafb221717dcfbffff247a9db8882c7801923db4
MKachi <mkachi@naver.com>
2020-08-23 02:53:25 +0900

    Change Text component style

--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import styles from './style.module.css'
 
 interface IProps {
   className?: string
@@ -10,6 +11,22 @@ interface IProps {
   strike?: boolean
 }
 
-const Text: React.FC<IProps> = ({ className, children, bold = false, italic = false, underline = false, strike = false }) => {
-  return <span>{children}</span>
+const Text: React.FC<IProps> = ({
+  className,
+  children,
+  bold = false,
+  italic = false,
+  underline = false,
+  strike = false,
+}) => {
+  const classProps = classNames(
+    bold ? styles['bold'] : '',
+    italic ? styles['italic'] : '',
+    underline ? styles['underline'] : '',
+    strike ? styles['strike'] : ''
+  )
+
+  return <span className={classProps}>{children}</span>
 }
+
+export default Text
