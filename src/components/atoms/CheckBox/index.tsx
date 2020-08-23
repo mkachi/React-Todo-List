3316df04f6c5197148c0110f09960da3bef787da
MKachi <mkachi@naver.com>
2020-08-24 00:41:43 +0900

    Remote CheckBox component state

--- src/components/atoms/CheckBox/index.tsx
+++ src/components/atoms/CheckBox/index.tsx
@@ -13,22 +13,20 @@ interface IProps {
 }
 
 const CheckBox: React.FC<IProps> = ({ className, checked = false, color = '#788cde', onChange = () => {} }) => {
-  const [isChecked, setCheck] = useState(checked)
   const classProps = classNames(className, styles['default'])
-  const StyledSpan = styled(isChecked, color)
+  const StyledSpan = styled(checked, color)
 
   return (
     <StyledSpan className={classProps}>
       <input
         className={styles['check-input']}
         type={'checkbox'}
-        checked={isChecked}
+        checked={checked}
         onChange={(event: React.FormEvent<HTMLInputElement>) => {
-          setCheck(event.target.checked)
           onChange(event)
         }}
       />
-      {isChecked && <AiOutlineCheck className={styles['check-icon']} />}
+      {checked && <AiOutlineCheck className={styles['check-icon']} />}
     </StyledSpan>
   )
 }
