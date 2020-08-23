3d4a330a30ffe2de35e9ce92c6245640fcd49c1a
MKachi <mkachi@naver.com>
2020-08-24 03:08:14 +0900

    Add TodoInput component

---
+++ src/components/organisms/TodoInput/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+import Card from '../../molecules/Card'
+import TextBox from '../../atoms/TextBox'
+import Button from '../../atoms/Button'
+
+import { GrAdd } from 'react-icons/gr'
+
+interface IProps {
+  className?: string
+  placeholder?: string
+  text?: string
+  onClick?(event: React.FormEvent<HTMLButtonElement>): void
+  onChange?(event: React.FormEvent<HTMLInputElement>): void
+}
+
+const TodoInput: React.FC<IProps> = ({
+  className,
+  placeholder = '',
+  text = '',
+  onClick = () => {},
+  onChange = () => {},
+}) => {
+  const classProps = classNames(className, styles['default'])
+  return (
+    <Card className={classProps} element={'div'} color={'#36bdb44f'}>
+      <TextBox className={styles['input']} placeholder={placeholder} value={text} onChange={onChange} />
+      <Button className={styles['add-button']} variant={'text'} color={'#ffffff'} onClick={onClick}>
+        <GrAdd />
+      </Button>
+    </Card>
+  )
+}
+
+export default TodoInput
