1f0bf720f7d36585956a8f4f66666d0bdeb44fcc
MKachi <mkachi@naver.com>
2020-09-21 14:25:03 +0900

    Write Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,8 @@ import React, { useContext, useState } from 'react'
 import styles from './style.module.css'
 
 import Container from '../../components/molecules/Container'
-import Card from '../../components/molecules/Card'
-import P from '../../components/atoms/P'
 import TodoCard from '../../components/organisms/TodoCard'
-import TodoInput from '../../components/organisms/TodoInput'
+import TodoInput from '../../components/templates/TodoInput'
 import Flex from '../../components/molecules/Flex'
 import { TodoContext } from '../../store/Todo'
 
@@ -16,7 +14,7 @@ const Home = () => {
     <Container>
       <Flex element={'section'} className={styles['list']} direction={'column'}>
         {items.map((todo, index) => {
-          return <TodoCard key={index} data={todo} />
+          return <TodoCard key={index} data={todo} onToggle={() => setToggle(todo.id, !todo.checked)} onRemove={() => remove(todo.id)} />
         })}
       </Flex>
       <footer
@@ -29,6 +27,13 @@ const Home = () => {
           text={inputText}
           onChange={(event: React.FormEvent<HTMLInputElement>) => {
             setInputText(event.target.value)
+          }}
+          onClick={() => {
+            if (inputText === '') {
+              return
+            }
+            add(inputText)
+            setInputText('')
           }}
         />
       </footer>
