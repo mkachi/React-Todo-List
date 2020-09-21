69985b8fe1365c35b1536d96791dfe3599ff4a38
MKachi <mkachi@naver.com>
2020-09-21 14:30:46 +0900

    Add onKeyPress event

--- src/components/atoms/TextBox/index.tsx
+++ src/components/atoms/TextBox/index.tsx
@@ -7,11 +7,12 @@ interface IProps {
   placeholder?: string
   value?: string
   onChange?(event: React.FormEvent<HTMLInputElement>): void
+  onKeyPress?(event: React.KeyboardEvent<HTMLInputElement>): void
 }
 
-const TextBox: React.FC<IProps> = ({ className, placeholder = '', value = '', onChange = () => {} }) => {
+const TextBox: React.FC<IProps> = ({ className, placeholder = '', value = '', onChange = () => {}, onKeyPress = () => {} }) => {
   const classProps = classNames(className, styles['default'])
-  return <input className={classProps} type={'textbox'} placeholder={placeholder} value={value} onChange={onChange} />
+  return <input className={classProps} type={'textbox'} placeholder={placeholder} value={value} onChange={onChange} onKeyPress={onKeyPress} />
 }
 
 export default TextBox
